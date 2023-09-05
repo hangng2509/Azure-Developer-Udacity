261c58f8ff7d2f0518827944ec0bd8585c8f5a86
hangng2509 <hn192509@gmail.com>
2023-09-06 01:36:26 +0700

    Lab1: Update scope

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from flask import render_template, flash, redirect, request, session, url_for
 from werkzeug.urls import url_parse
 from config import Config
-from FlaskWebProject import app, db, logger as app_logger
+from FlaskWebProject import app, db
 from FlaskWebProject.forms import LoginForm, PostForm
 from flask_login import current_user, login_user, logout_user, login_required
 from FlaskWebProject.models import User, Post
@@ -40,7 +40,7 @@ def new_post():
     if form.validate_on_submit():
         post = Post()
         post.save_changes(form, request.files['image_path'], current_user.id, new=True)
-        app_logger.info("Creating post successfully")
+        logger.info("Creating post successfully")
         return redirect(url_for('home'))
     return render_template(
         'post.html',
@@ -74,17 +74,17 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            app_logger.info("Invalid login attempt")
+            logger.info("Invalid login attempt")
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
-            app_logger.info("admin logged in successfully")
+            logger.info("admin logged in successfully")
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
-    auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
+    auth_url = _build_auth_url(scopes=["User.Read"], state=session["state"])
     return render_template('login.html', title='Sign In', form=form, auth_url=auth_url)
 
 
